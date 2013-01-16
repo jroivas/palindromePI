ec5fe9614fc955f2a65cb86165f24cc119ac2696
Jouni Roivas <jroivas@iki.fi>
2013-01-16 06:17:17 +0200

    Faster algorithm, consume less memory, support for Python 3.

--- findpi.py
+++ findpi.py
@@ -1,74 +1,130 @@
 #!/usr/bin/python
 import math
 
-def primes(n): 
-	if n==2: return [2]
-	elif n<2: return []
-	s=range(3,n+1,2)
-	mroot = n ** 0.5
-	half=(n+1)/2-1
-	i=0
-	m=3
-	while m <= mroot:
-		if s[i]:
-			j=(m*m-3)/2
-			s[j]=0
-			while j<half:
-				s[j]=0
-				j+=m
-		i=i+1
-		m=2*i+3
-	return [2]+[x for x in s if x]
+def primes(n, minimum): 
+    """ Calculate primes up to given number
+    Filter out smaller than minimum primes
 
-def isPalindrome(s):
-	c=len(s)
-	if c<=1: return False
-	if c==2:
-		if s[0] == s[1]: return True
-		else: return False
+    >>> primes(2, 0)
+    [2]
+    >>> primes(10, 0)
+    [2, 3, 5, 7]
+    >>> primes(100, 50)
+    [53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    """
+    if n == 2:
+        return [2]
+    elif n < 2:
+        return []
 
-	m = int(c/2)
-	for n in range(0,m):
-		if s[n] != s[c-n-1]: return False
+    s = list(range(3, n + 1, 2))
+    mroot = n ** 0.5
+    half = (n + 1)/2 - 1
+    i = 0
+    m = 3
+    while m <= mroot:
+        if s[i]:
+            j = int((m * m - 3) / 2)
+            s[j] = 0
+            while j < half:
+                s[j] = 0
+                j += m
+        i = i + 1
+        m = 2 * i + 3
 
-	return True
+    if minimum <= 2:
+       res = [2] 
+    else:
+        res = []
+    res += [x for x in s if x and x >= minimum]
+    return res
 
-def filterPalindrome(n,minlen):
-	res = []
-	for i in n:
-		s="%d"%i
-		c=len(s)
-		if (c>=minlen):
-			if isPalindrome(s):
-				res = res+[i]
-	return res
+def isPalindrome(i):
+    """ Check if number is palindrome
 
-def readPi():
-	# Assume we have precalc'd pi
-	# This one is from: http://www.archive.org/stream/Pi_to_100000000_places/pi.txt
-	pi = ""
-	p = open("pi.txt")
-	line = p.readline()
-	while 1:
-		if not line: break
-		pi += line.strip().replace(' ','')
-		line = p.readline()
-		
-	p.close()
-	return pi
+    >>> isPalindrome(99)
+    True
+    >>> isPalindrome(999)
+    True
+    >>> isPalindrome(9990)
+    False
+    >>> isPalindrome(1991)
+    True
+    >>> isPalindrome(9999)
+    True
+    >>> isPalindrome(99199)
+    True
+    >>> isPalindrome(4242)
+    False
+    >>> isPalindrome(4224)
+    True
+    >>> isPalindrome(123454321)
+    True
+    >>> isPalindrome(123454311)
+    False
+    """
 
-# Read PI value
-mypi = readPi()
+    if i < 10:
+        return False
+    if i < 100:
+        a = int(i / 10)
+        b = i % 10
+        if a == b:
+            return True
+        return False
 
-# Filter primes under 9999999, min length 7
-primes = filterPalindrome(primes(9999999),7)
+    s = str(i)
+    c = len(s)
+    m = int(c / 2)
+    a = s[:m]
+    if m * 2 == c:
+        b = s[m:]
+    else:
+        b = s[m + 1:]
+    b = b[::-1]
 
-best = 99999999
-val = -1
-for i in primes:
-	pos = mypi.find("%d"%i)
-	if pos>0 and best>pos:
-		best = pos
-		val = i
+    return a == b
 
-print val
+def filterPalindrome(n, minlen):
+    for i in n:
+        if i >= minlen and isPalindrome(i):
+            yield i
+
+def searchPalindromeFromPi(primes):
+    best = maxnum
+    val = -1
+
+    strs = [str(i) for i in primes]
+
+    with open('pi.txt', 'r') as p:
+        prev = ''
+        while True:
+            line = p.readline()
+            if not line:
+                break
+            line = line.strip().replace(' ', '')
+            haystack = prev + line
+            candidates = []
+            for i in strs:
+                pos = haystack.find(i)
+                if pos > 0:
+                    candidates.append((pos, i))
+            if candidates:
+                res = sorted(candidates)
+                val = res[0][1]
+                return val
+            prev = line
+
+    return val
+
+if __name__ == '__main__':
+    # Filter primes under 9999999, min length 7
+    numlen = 7
+    minnum = int('1' + '0' * (numlen - 1))
+    maxnum = int('9' * numlen)
+    primes = filterPalindrome(primes(maxnum, minnum), minnum)
+
+    # Search proper palindrome prime from Pi
+    val = searchPalindromeFromPi(primes)
+
+    print (val)
